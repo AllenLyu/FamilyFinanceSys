e20bfe31299c718f8058933598fddb9eccbdfcd8
AllenLyu <allen.walker.lc@gmail.com>
2015-04-26 21:32:50 +0800

    添加了主界面
    主界面添加

---
+++ src/Login.java
@@ -0,0 +1,159 @@
+/*
+ * Login.java
+ *
+ * Created on __DATE__, __TIME__
+ */
+
+/**
+ *
+ * @author  __USER__
+ */
+public class Login extends javax.swing.JFrame {
+
+	/** Creates new form Login */
+	public Login() {
+		initComponents();
+	}
+
+	//GEN-BEGIN:initComponents
+	// <editor-fold defaultstate="collapsed" desc="Generated Code">
+	private void initComponents() {
+
+		jLabel1 = new javax.swing.JLabel();
+		jTextField1 = new javax.swing.JTextField();
+		jLabel2 = new javax.swing.JLabel();
+		jTextField2 = new javax.swing.JTextField();
+		jButton1 = new javax.swing.JButton();
+		jButton2 = new javax.swing.JButton();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+		jLabel1.setText("\u7528\u6237\u540d\uff1a");
+
+		jLabel2.setText("\u5bc6\u7801\uff1a");
+
+		jButton1.setText("\u767b\u9646");
+
+		jButton2.setText("\u6ce8\u518c");
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		getContentPane().setLayout(layout);
+		layout
+				.setHorizontalGroup(layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								layout
+										.createSequentialGroup()
+										.addGap(63, 63, 63)
+										.addGroup(
+												layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING,
+																false)
+														.addGroup(
+																layout
+																		.createSequentialGroup()
+																		.addComponent(
+																				jButton1)
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+																				javax.swing.GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				jButton2))
+														.addGroup(
+																layout
+																		.createSequentialGroup()
+																		.addGroup(
+																				layout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.LEADING)
+																						.addComponent(
+																								jLabel1)
+																						.addComponent(
+																								jLabel2))
+																		.addGap(
+																				36,
+																				36,
+																				36)
+																		.addGroup(
+																				layout
+																						.createParallelGroup(
+																								javax.swing.GroupLayout.Alignment.LEADING,
+																								false)
+																						.addComponent(
+																								jTextField2)
+																						.addComponent(
+																								jTextField1,
+																								javax.swing.GroupLayout.DEFAULT_SIZE,
+																								185,
+																								Short.MAX_VALUE))))
+										.addContainerGap(68, Short.MAX_VALUE)));
+		layout
+				.setVerticalGroup(layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								layout
+										.createSequentialGroup()
+										.addGap(70, 70, 70)
+										.addGroup(
+												layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.BASELINE)
+														.addComponent(jLabel1)
+														.addComponent(
+																jTextField1,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																javax.swing.GroupLayout.DEFAULT_SIZE,
+																javax.swing.GroupLayout.PREFERRED_SIZE))
+										.addGap(62, 62, 62)
+										.addGroup(
+												layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.BASELINE)
+														.addComponent(jLabel2)
+														.addComponent(
+																jTextField2,
+																javax.swing.GroupLayout.PREFERRED_SIZE,
+																javax.swing.GroupLayout.DEFAULT_SIZE,
+																javax.swing.GroupLayout.PREFERRED_SIZE))
+										.addPreferredGap(
+												javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+												50, Short.MAX_VALUE)
+										.addGroup(
+												layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.BASELINE)
+														.addComponent(jButton1)
+														.addComponent(jButton2))
+										.addGap(47, 47, 47)));
+
+		pack();
+	}// </editor-fold>
+	//GEN-END:initComponents
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Login().setVisible(true);
+			}
+		});
+	}
+
+	//GEN-BEGIN:variables
+	// Variables declaration - do not modify
+	private javax.swing.JButton jButton1;
+	private javax.swing.JButton jButton2;
+	private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabel2;
+	private javax.swing.JTextField jTextField1;
+	private javax.swing.JTextField jTextField2;
+	// End of variables declaration//GEN-END:variables
+
+}
