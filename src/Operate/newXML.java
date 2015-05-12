c7c0b1647e5b0a592ca1c415e818e9699d2202c5
itas1994 <itas1994@sina.com>
2015-05-12 23:25:18 +0800

    更新生成新xml文件的类
    根据用户名生成相应xml文件

---
+++ src/Operate/newXML.java
@@ -0,0 +1,40 @@
+package Operate;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import DOM.Finance;
+
+public class newXML {
+		public static void creatNewXml(String userid) throws ParserConfigurationException, TransformerException, SAXException, IOException{
+			String filepath="D:/MyEclipse 8.5Workspace/FamilyFinanceSys/"+userid+".txt";
+			DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder=factory.newDocumentBuilder();
+			Document doc=builder.newDocument();
+			Element Finances=doc.createElement("Finances");
+			doc.appendChild(Finances);
+			
+			Transformer transformer=TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8"); 
+			StreamResult fileResult=new StreamResult(new File(filepath));
+			DOMSource source=new DOMSource(doc);
+			transformer.transform(source,fileResult);
+		}
+}
+
